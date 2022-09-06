efe5a71596663e4d83f59ee46637df7744ad4d89
CasBlease <charlieandthatsit@gmail.com>
2022-09-06 02:06:28 -0500

    Update lsketch.js

--- lsketch.js
+++ lsketch.js
@@ -3,8 +3,8 @@ function setup(){
 
     m=2
     l=windowHeight/2
-    g=10*100
-    dt=0.02
+    g=10*100 //scale gravity up from pixels
+    dt=0.02 //set draw speed
     t=0
     theta = 1/2; // Pendulum initial angle theta
     omega = 0; // Initial angular velocity
@@ -12,7 +12,7 @@ function setup(){
 }
 
 function draw(){
-    background(220);
+    background(153, 101, 21);
     // physics
     t = t + dt;
     F=-m*g*sin(theta)
@@ -21,7 +21,11 @@ function draw(){
     theta = theta + omega * dt;
     xp = C - l * sin(theta); // X coordinate of pendulum ball
     yp = l * cos(theta); // Y coordinate of pendulum ball
+    
     //draw it
+    stroke(207, 181, 59);
+    strokeWeight(5);
     line(C, 0, xp, yp);
+    fill(255, 223, 0);
     ellipse(xp, yp, 50, 50);
 }
